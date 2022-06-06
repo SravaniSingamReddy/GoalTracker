b36091a96a5ed87c1a3279dedf3ee984d2061ea3
Singam Sravani <72649538+SINGAMSRAVANI-A@users.noreply.github.com>
2022-06-06 15:13:37 +0530

    Add files via upload

---
+++ Frontend/src/components/login.js
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+import axios from "axios";
+const Login=(props)=>{
+    const [email,setEmail]=useState("");
+    const [password,setPassword]=useState("");  
+    function handleLogin(e){
+        e.preventDefault();
+        console.log("heee")
+        axios({
+            method:'post',
+            url:'/login',
+            contentType:'application/json',
+            data:{
+                email,
+                password
+            }
+        }).then((response)=>{
+            console.log(response.data);
+        }).catch((err)=>{            
+            console.log(err);
+        });
+    }
+    return(
+        <div className="login">
+            <form className="login-form" onSubmit={handleLogin}>
+            <div className="login-form-header">
+                <h2>Log In</h2>
+            </div>
+            <div className="login-form-fields">
+                <label>Email</label>
+                <input type="email" onChange={(e)=>setEmail(e.target.value)} value={email} placeholder="Enter Email" required/>
+            </div>
+            <div className="login-form-fields">
+                <label>Password</label>
+                <input type="password" onChange={(e)=>setPassword(e.target.value)} value={password} placeholder="Enter password" required/>
+            </div>
+            <div className="not-login">
+                <p >Not Registered? Sign Up here</p>
+            </div>
+            <div className="login-form-btn">
+                <button type="submit">Sign In</button>
+            </div>
+        </form>
+        </div>
+        
+    )
+}
+
+export default Login;
