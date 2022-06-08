d1f76b4683edb34df4bc8d14fa721470ccf56451
Singam Sravani <72649538+SINGAMSRAVANI-A@users.noreply.github.com>
2022-06-08 18:57:07 +0530

    Update login.js

--- Backend/routers/login.js
+++ Backend/routers/login.js
@@ -7,45 +7,44 @@ const Users = require("../models").users;
 const { body, validationResult } = require("express-validator");
 
 const validate = [
-  body("email").isEmail().withMessage("Enter valid email"),
-  body("password").notEmpty().withMessage("Password not be empty"),
+  body("email").notEmpty().withMessage("Email should not be empty"),
+  body("password").notEmpty().withMessage("Password should not be empty"),
 ];
 router.post("/", jsonParser, validate, async (req, res) => {
   const password = req.body.password;
   const email = req.body.email;
-  console.log(password)
-  console.log(email)  
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     return res.status(400).json({ errors: errors.array() });
   } else {
-    const checkEmail = await Users.findOne({ where: { email } });
-    if(!checkEmail){
+    const userDetails = await Users.findOne({ where: { email } });
+    if(!userDetails){
       res.status(400).json({ error: "Email id is incorrect" });    
-    }else{    
-    const userDetails = await Users.findOne({ where: { email, password } });
-    if (!userDetails) {
-      res.status(400).json({ error: "Password is incorrect" });
-    } else {
-      const payload = {};
-      console.log(`user details: ${JSON.stringify(userDetails)}`);
-      const role = userDetails.role;      
-      if (role == "Employee") {
-        payload.isEmployee = true;
-        payload.isAdmin = false;
-        payload.isSuperAdmin = false;
-      } else if (role == "Admin") {
-        payload.isEmployee = true;
-        payload.isAdmin = true;
-        payload.isSuperAdmin = false;
-      } else if (role == "Super Admin") {
-        payload.isEmployee = true;
-        payload.isAdmin = true;
-        payload.isSuperAdmin = true;
+    }else{   
+      if(userDetails.email===email&&userDetails.password===password){
+        const payload = {};
+        console.log(`user details: ${JSON.stringify(userDetails)}`);
+        const role = userDetails.role;      
+        if (role == "Employee") {
+          payload.isEmployee = true;
+          payload.isAdmin = false;
+          payload.isSuperAdmin = false;
+        } else if (role == "Admin") {
+          payload.isEmployee = true;
+          payload.isAdmin = true;
+          payload.isSuperAdmin = false;
+        } else if (role == "Super Admin") {
+          payload.isEmployee = true;
+          payload.isAdmin = true;
+          payload.isSuperAdmin = true;
+        }
+        console.log(`payload: ${JSON.stringify(payload)}`);
+        const token = jwt.sign(payload, "secret123");
+        res.json({ jwt: token ,userDetails:userDetails});
+      
       }
-      console.log(`payload: ${JSON.stringify(payload)}`);
-      const token = jwt.sign(payload, "secret123");
-      res.json({ jwt: token });
+    else  {
+      res.status(400).json({ error: "Password is incorrect" });
     }
   }
 }
