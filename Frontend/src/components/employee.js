7b7bfcd02c66286a7a31531098ee54156df2bb03
Singam Sravani <72649538+SINGAMSRAVANI-A@users.noreply.github.com>
2022-06-08 18:48:56 +0530

    Delete Frontend directory

--- Frontend/src/components/employee.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Employee = () => {
-  return (
-    <div>Employee page</div>
-  )
-}
-
-export default Employee;
--- Frontend/src/components/superadmin.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const SuperAdmin = () => {
-  return (
-    <div>superadmin page</div>
-  )
-}
-
-export default SuperAdmin;
